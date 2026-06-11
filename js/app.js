20052a4d0f35f6bd814645cd986f2474428ef9a6
zcc842154520 <48348882+zcc842154520@users.noreply.github.com>
2026-06-11 03:29:31 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -519,7 +519,7 @@ async function search(isInit = false) {
                 }
                 
                 const controller = new AbortController();
-                const timeoutId = setTimeout(() => controller.abort(), 15000);
+                const timeoutId = setTimeout(() => controller.abort(), 30000);
                 
                 const response = await fetch(PROXY_URL + encodeURIComponent(apiUrl), {
                     headers: API_CONFIG.search.headers,
